bf0aa386766f32ec27dea9eb03481314bcabec46
mikag <yahligoldenberg@gmail.com>
2021-09-21 16:55:42 +0300

    class main number

--- src/main.java
+++ src/main.java
@@ -1,10 +1,17 @@
-class Main {
-    public static void main(String[]args) {
-        int number1 = 10;
-        int number2 = 6;
-        int sum;
-        sum = number1 + number2;
-        System.out.println(sum);
+import java.util.Scanner;
 
+class Main{
+
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+
+        double d1,d2,sum;
+        System.out.println("Enter the 1 st number.");
+        d1 = in.nextDouble();
+        System.out.println("Enter the 2 nd number.");
+        d2 = in.nextDouble();
+        sum = d1 + d2;
+        System.out.println("The sum is:" + sum);
     }
 }

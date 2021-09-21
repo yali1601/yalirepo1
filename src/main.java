e805a2b1450afc614e9cde0f6db0e92be39366ea
mikag <yahligoldenberg@gmail.com>
2021-09-21 11:18:41 +0300

    class main number

--- src/main.java
+++ src/main.java
@@ -1,3 +1,10 @@
-public static void main {
+class Main {
+    public static void main(String[]args) {
+        int number1 = 10;
+        int number2 = 6;
+        int sum;
+        sum = number1 + number2;
+        System.out.println(sum);
 
+    }
 }

c7b9a248214eaedd1b1858d1c0edd50ea750ab2a
yali1601 <91138267+yali1601@users.noreply.github.com>
2021-09-21 22:02:52 +0300

    class main number

---
+++ src/Main2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main2 {
+
+    public static void main(String[] args){
+
+        int n1 = 10, n2 = 3 ;
+
+        System.out.print((double) n1 / n2);
+    }
+}
